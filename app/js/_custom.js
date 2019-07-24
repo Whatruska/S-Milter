fa95e82be71a7218c67ad09077a707c1e1b0a88b
Vitalii Dubrovets <vitdub@inbox.ru>
2019-07-24 20:07:08 +0600

    side menu added

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
+	$('#my-menu').mmenu({
+		extensions:[
+			'widescreen',
+			'theme-black',
+			'effect-menu-slide',
+			'pagedim-black',
+			'position-right'
+		],
+		navbar : {
+			title : '<img src="img/logo-1.svg">'
+		}
+	});
 
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function(){
+		$('.hamburger').addClass('is-active');
+	});
+	api.bind('close:finish', function () {
+		$('.hamburger').removeClass('is-active');
+	});
+	$('.mm-listitem_selected i').removeClass('far').addClass('fas');
 	// Custom JS
 
 });
